45a8abaee75cbd348e52b9b8410996ac6532e88d
Yurii Yakubin <yurii.yakubin@gmail.com>
2023-03-19 15:18:27 +0000

    CMake build for jbig2dec-0.19

---
+++ Packages/jbig2dec/0.19/config.h.cmake
@@ -0,0 +1,8 @@
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine HAVE_STRING_H 1
+#cmakedefine HAVE_GETOPT_LONG 1
+#cmakedefine HAVE_LIBPNG 1
+
+# if defined(WIN32) && !(defined(inline))
+#  define inline __inline
+# endif
